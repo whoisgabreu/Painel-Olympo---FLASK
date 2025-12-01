5fad61e16c8cd5973c7767d9826534087ccc007b
Gabriel Lucas de Ângelus <150370647+whoisgabreu@users.noreply.github.com>
2025-12-01 15:54:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -725,6 +725,10 @@ def classificacao():
             if resposta in regras:
                 resultado = regras[resposta]
                 respostas["resultado"] = resultado
+                ## TESTE
+                resposta["pontos_positivos"] = analise_ia.json()[0]["pontos_positivos"]
+                resposta["pontos_negativos"] = analise_ia.json()[0]["pontos_negativos"]
+                ## FIM TESTE
                 rq.post(url = "https://n8n.v4lisboatech.com.br/webhook/analise/registrar-forms", json = respostas)
                 return render_template(
                     "resultado_classificacao.html",
@@ -801,4 +805,5 @@ def apresentacao():
 # -----------------------------
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5001, debug=True)
+
 
