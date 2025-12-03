6d8947f3e2325e9fe9aac7d8387af0fd84a60570
Gabreu <gabreumartins02@gmail.com>
2025-12-03 15:26:57 -0300

    Colocando load spinner na tela do Radar de Variável

--- app.py
+++ app.py
@@ -819,8 +819,4 @@ def site_map():
 # RUN
 # -----------------------------
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5001, debug = True)
-
-
-
-
+    app.run("0.0.0.0", port=5001, debug = False)
